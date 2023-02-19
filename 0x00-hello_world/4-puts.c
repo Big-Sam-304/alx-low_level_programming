6be9eb465f0d74ddff52cf990dd9e6eaf40ad758
Samuel <njengasam53@gmail.com>
2023-02-19 03:39:49 -0800

    print a line in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programe that prints out a line using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
