9d451ec6d2e1c788950520e901c3ed17f9bb9329
Samuel <njengasam53@gmail.com>
2023-02-19 04:42:28 -0800

    simplicity of UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints out an error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
