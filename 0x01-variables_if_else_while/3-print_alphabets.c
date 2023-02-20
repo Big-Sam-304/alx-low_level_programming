0576a78e4771569ac957cb6a3b4f223705e7e80e
Samuel <njengasam53@gmail.com>
2023-02-20 13:36:00 -0800

    Alphabet game with lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Prints out the alphabet n lower and
+ * upper case.
+ *
+ * Returns: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+
+}
