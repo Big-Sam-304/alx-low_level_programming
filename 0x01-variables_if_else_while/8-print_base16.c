f6bf34162271ec044c303db3a026a61c650d4e93
Samuel <njengasam53@gmail.com>
2023-02-20 15:33:35 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints out base 16 using putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	char hex[] = {'0', '1', '2', '3', '4', '5', '6',
+		'7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hex[i]);
+	}
+	putchar('\n');
+	return (0);
+}
