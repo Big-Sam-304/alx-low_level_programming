40d103aa152a5ea67bc91635cf67c688106b2b9b
Samuel <njengasam53@gmail.com>
2023-02-21 16:49:23 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints out a letter
+ * 10 times before moving to a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		int i;
+
+		for (i = 0; i < 10; i++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
