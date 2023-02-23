17b84d4966e1dcf489b548857b64de4a4c8f000a
Samuel <njengasam53@gmail.com>
2023-02-23 15:46:35 -0800

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "mass.h"
+/**
+ * print_most_numbers - Check code
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if !(i == 2 || 1 == 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
