fcb337362f677009fda368e3cdfc29221f79f7f2
Samuel <njengasam53@gmail.com>
2023-02-28 14:50:10 -0800

    half n half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#insert "main.h"
+/**
+ * puts_half - Prints half a string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int len, length_of_the_string, i, n;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++
+	}
+	if (len % 2 == 0)
+	{
+		mid = len / 2;
+		for (i = mid; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		length_of_the_string = len;
+
+		n = (length_of_the_string - 1) / 2;
+		for (i = n; i <= length_of_the_string - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
