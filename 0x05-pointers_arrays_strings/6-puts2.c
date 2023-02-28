21f429cb863634e0660173dad942648f18c365ef
Samuel <njengasam53@gmail.com>
2023-02-28 14:08:08 -0800

    print a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "mass.h"
+/**
+ * puts2 - prints out characters in a string.
+ * @str: String
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
