e7d0ba5e8e959a9c4d8d92e7244361f8b1299336
Samuel <njengasam53@gmail.com>
2023-03-01 09:11:26 -0800

    Two Strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: dest (success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	int i;
+
+	for (i = 0; i < n && *src[i] != '\0'; i++)
+	{
+		*p++ = src[i];
+	}
+	*p = '\0';
+	return (dest);
+}
