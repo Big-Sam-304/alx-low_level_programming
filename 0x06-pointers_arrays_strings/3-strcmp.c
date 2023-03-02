08ce6ac5281c3fa0a81c66eb8ff68593aabd83c9
Samuel <njengasam53@gmail.com>
2023-03-02 10:58:22 -0800

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,23 +4,25 @@
  * @s1: string
  * @s2: string
  * Return: 0 if two strings are the same,
- * return -15 if s2 is greater than s1
- * return 15 if s1 is greater than s2
+ * return -(negative) if s2 is greater than s1
+ * return +(positive) if s1 is greater than s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	while (i != '\0' || i != '\0')
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] < s2[i])
 		{
-			return (-15);
+			return (s1[i] - s2[i]);
 		}
 		else if (s1[i] > s2[i])
 		{
-			return (15);
+			return (s1[i] - s2[1]);
 		}
 		i++;
 	}
