ba3ead882921c176297c442bb8c3d122b2e40913
Samuel <njengasam53@gmail.com>
2023-03-02 11:34:43 -0800

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,25 +14,9 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-		if (s1[i] < s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (s1[i] - s2[1]);
-		}
 		i++;
 	}
-	if (s1[i] == '\0')
-	{
-		return (-1);
-	}
-	else if (s2[i] == '\0')
-	{
-		return (1);
-	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
