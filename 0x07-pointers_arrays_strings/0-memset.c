2952ba1a78431ecaf768692119d275e531ac1f62
Samuel <njengasam53@gmail.com>
2023-03-07 12:55:24 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - Fills the first n bytes of the memoty with a constant byte
+ * @s: memory area
+ * @b: constant
+ * @n: integer
+ * Return: s (success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n-- > 0)
+	{
+		*s++ = b;
+	}
+	_putchar('\n');
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+#POINTERS
+## Learning objectives
+- What are pointers and how to use them
+- What are multidimensional arrays and how to use them
+- What are the most common C standard library functions to manipulate strings
