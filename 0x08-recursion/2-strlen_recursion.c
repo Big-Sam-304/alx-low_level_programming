e9de4fc1020ed214754228339d0e6f81f2fac0c6
Samuel <njengasam53@gmail.com>
2023-03-08 04:14:04 -0800

    Length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,13 +10,12 @@ int _strlen_recursion(char *s)
 {
 	int count;
 
-	count = 0;
+	count = 1;
 	if (*s == '\0')
 	{
 		return (0);
 	}
-	count++;
-	_strlen_recursion(s + 1);
+	count = count + _strlen_recursion(s + 1);
 
 	return (count);
 }
