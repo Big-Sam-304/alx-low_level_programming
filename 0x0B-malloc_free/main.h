5cf95b278063919eae2f43a172d1cd3fc3a083a0
Samuel <njengasam53@gmail.com>
2023-03-19 00:12:20 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,6 +9,6 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 int _putchar(char c);
-int len_string(char *str)
+int len_string(char *str);
 
 #endif
