89bb56427a6f69caa1693d3725e1309e5e58f2b6
Samuel <njengasam53@gmail.com>
2023-04-04 08:17:58 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	current_node = *head;
 	new_node->next = NULL;
-	counter = 0;
 
 	if (idx == 0)
 	{
@@ -37,6 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
+	counter = 1;
 
 	while (current_node)
 	{
