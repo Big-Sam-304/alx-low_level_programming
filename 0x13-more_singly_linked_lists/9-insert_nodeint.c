2f05fa846e578cbc432ea2bcb9be071fbaa77694
Samuel <njengasam53@gmail.com>
2023-04-04 08:20:54 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	counter = 1;
+	counter = 0;
 
 	while (current_node)
 	{
