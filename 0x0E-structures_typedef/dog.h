cf1d84e9eae1229afd600d80fe67112682ab4daf
Njenga Gikaru <njengasam53@gmail.com>
2023-06-19 15:40:38 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type dog about a dog
+ * @name: name of dog
+ * @age: age of th dog
+ * @owner: owner of the dog
+ *
+ * Description: This data types stores all the info we
+ * need about our dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
