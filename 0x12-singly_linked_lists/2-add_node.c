23160332c76d8aa0bef1a4f5447e613613e46518
Njenga Gikaru <njengasam53@gmail.com>
2023-07-01 16:18:12 -0700

    singly lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,7 +28,7 @@ list_t *add_node(list_t **head, const char *str)
 		len++;
 	}
 
-	new_node = (list_t *) malloc(sizeof(list_t));
+	new_node = (list_t *)malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
