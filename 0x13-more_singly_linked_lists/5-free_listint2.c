102d45447f31c0822a342f7a56b8e6773f427804
Njenga Gikaru <njengasam53@gmail.com>
2023-07-04 00:05:39 -0700

    singly lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t
+ * @head: first node
+ *
+ * Return: nothing
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	*head = NULL;
+}
