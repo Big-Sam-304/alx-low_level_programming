47af8161c4b4edc04bdbd4f9f793bb2dcb04e096
Njenga Gikaru <njengasam53@gmail.com>
2023-07-11 15:13:04 -0700

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: text file to be read
+ * @letters: number of letters to be read from the file
+ *
+ * Return: actural number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t letters_r, total_letters_r = 0;
+
+	char buffer[1024];
+
+	FILE *file = fopen(filename, "r");
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	if (file == NULL)
+	{
+		return (0);
+	}
+
+	while (total_letters_r < (ssize_t)letters &&
+		(letters_r = fread(buffer, sizeof(char), sizeof(buffer), file)) > 0)
+	{
+
+		if (fwrite(buffer, sizeof(char), letters_r, stdout) != (long unsigned int)letters_r)
+		{
+			fclose(file);
+			return (0);
+		}
+
+		total_letters_r += letters_r;
+
+	}
+
+	fclose(file);
+	return (total_letters_r);
+}
