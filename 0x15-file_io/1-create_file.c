89943d39888e84e241de16807467d98afd41ea8a
Njenga Gikaru <njengasam53@gmail.com>
2023-07-12 07:14:05 -0700

    File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to be created, or truncated if it exists
+ * @text_content: content to be written into the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_w, file_flags, file_permissions;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	file_flags = O_WRONLY | O_CREAT | O_TRUNC;
+	file_permissions = S_IRUSR | S_IWUSR;
+
+	fd = open(filename, file_flags, file_permissions);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_w = write(fd, text_content, strlen(text_content));
+		if (bytes_w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (-1);
+}
